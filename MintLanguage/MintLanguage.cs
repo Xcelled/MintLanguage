69d2823dbaa1f473749ee74edc0d627e0fa148a5
X <j12@ketelaar.com>
2016-06-01 15:09:53 -0400

    Added basic expression

--- MintLanguage/MintLanguage.cs
+++ MintLanguage/MintLanguage.cs
@@ -20,20 +20,57 @@ namespace MintLanguage
 
             NumberLiteral number = new NumberLiteral("number");
             IdentifierTerminal identifier = new IdentifierTerminal("identifier");
-            KeyTerm refMark = ToTerm("&", "refMark");
+            StringLiteral backString = new StringLiteral("back-string", "`");
+            StringLiteral doubleString = new StringLiteral("double-string", "\"");
+            KeyTerm refMark = ToTerm("&", "ref-mark");
+            KeyTerm lPar = ToTerm("(");
+            KeyTerm rPar = ToTerm(")");
+            KeyTerm lBrace = ToTerm("{");
+            KeyTerm rBrace = ToTerm("}");
+            KeyTerm semi = ToTerm(";");
+            KeyTerm comma = ToTerm(",");
+            KeyTerm dot = ToTerm(".");
             #endregion
 
             #region Declare NonTerminals Here
+            NonTerminal literal = new NonTerminal("literal", number | backString | doubleString | "true" | "false");
+
             NonTerminal program = new NonTerminal("program");
             NonTerminal declarations = new NonTerminal("declarations");
             NonTerminal declaration = new NonTerminal("declaration");
+            NonTerminal method = new NonTerminal("method");
             NonTerminal modifier = new NonTerminal("modifier");
             NonTerminal typeRef = new NonTerminal("type-ref");
-            NonTerminal refMarkOpt = new NonTerminal("refmarkOpt", Empty | refMark);
+            NonTerminal refMarkOpt = new NonTerminal("ref-mark-opt", Empty | refMark);
             NonTerminal parenParameters = new NonTerminal("paren-parameters");
             NonTerminal parameters = new NonTerminal("parameters");
             NonTerminal parameter = new NonTerminal("parameter");
 
+            NonTerminal block = new NonTerminal("block");
+            NonTerminal statement = new NonTerminal("statement");
+            NonTerminal statementListOpt = new NonTerminal("statement-list-opt");
+            NonTerminal declarationStatement = new NonTerminal("declaration-statement");
+            NonTerminal embeddedStatement = new NonTerminal("embedded-statement");
+
+            NonTerminal variableDeclaration = new NonTerminal("variable-declaration");
+            NonTerminal variableDeclarator = new NonTerminal("variable-declarator");
+            NonTerminal variableDeclarators = new NonTerminal("variable-declarators");
+            NonTerminal initializer = new NonTerminal("initializer");
+
+
+
+            NonTerminal expression = new NonTerminal("expression");
+            NonTerminal typecast = new NonTerminal("typecast");
+            NonTerminal primaryExpression = new NonTerminal("primary-expression");
+            NonTerminal binOpExp = new NonTerminal("binary-operation");
+
+            NonTerminal memberAccess = new NonTerminal("member-access");
+            NonTerminal memberAccessSegment = new NonTerminal("member-access-segment");
+            NonTerminal memberAccessSegments = new NonTerminal("member-access-segments");
+
+            NonTerminal methodInvocation = new NonTerminal("method-invocation");
+            NonTerminal argList = new NonTerminal("arg-list");
+            NonTerminal arg = new NonTerminal("arg");
             #endregion
 
             #region Place Rules Here
@@ -43,17 +80,49 @@ namespace MintLanguage
 
             declarations.Rule = MakeStarRule(declarations, declaration);
 
-            declaration.Rule = modifier + typeRef + identifier + parenParameters;
+            declaration.Rule = method | declarationStatement;
+
+            method.Rule = modifier + typeRef + identifier + parenParameters + block;
 
             modifier.Rule = ToTerm("server") | "client";
+
             typeRef.Rule = identifier + refMarkOpt;
 
+            // Parameters
             parameter.Rule = typeRef + identifier;
-            parameters.Rule = MakeStarRule(parameters, ToTerm(","), parameter);
-            parenParameters.Rule = ToTerm("(") + parameters + ")";
+            parameters.Rule = MakeStarRule(parameters, comma, parameter);
+            parenParameters.Rule = lPar + parameters + rPar;
+
+            // Statements and blocks
+            block.Rule = lBrace + statementListOpt + rBrace;
+            statementListOpt.Rule = MakeStarRule(statementListOpt, null, statement);
+            statement.Rule = declarationStatement | embeddedStatement;
+
+            // Decalartion statement
+            declarationStatement.Rule = variableDeclaration + semi;
+            variableDeclaration.Rule = typeRef + variableDeclarators;
+            variableDeclarator.Rule = identifier + "=" + initializer;
+            variableDeclarators.Rule = MakePlusRule(variableDeclarators, comma, variableDeclarator);
+            initializer.Rule = expression;
+
+            // Embedded statement
+            embeddedStatement.Rule = block | semi;
+
+            // expression
+            expression.Rule = typecast | primaryExpression;
+            typecast.Rule = lPar + typeRef + rPar + primaryExpression;
+            primaryExpression.Rule = literal | identifier | memberAccess;
+
+            memberAccess.Rule = identifier + memberAccessSegments;
+            memberAccessSegments.Rule = MakePlusRule(memberAccessSegments, null, memberAccessSegment);
+            memberAccessSegment.Rule = dot + identifier | methodInvocation;
+
+            methodInvocation.Rule = lPar + argList + rPar;
+            argList.Rule = MakeStarRule(argList, comma, arg);
+            arg.Rule = expression;
             #endregion
 
-            MarkPunctuation("(", ")", ",");
+            MarkPunctuation(lPar, rPar, lBrace, rBrace, comma, semi, dot);
         }
     }
 }
