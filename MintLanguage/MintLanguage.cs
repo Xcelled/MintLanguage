625d73203d70bff0da67c2339052f73565f62f24
X <j12@ketelaar.com>
2016-06-01 23:29:48 -0400

    Prep for redesign

--- MintLanguage/MintLanguage.cs
+++ MintLanguage/MintLanguage.cs
@@ -27,6 +27,8 @@ namespace MintLanguage
             KeyTerm rPar = ToTerm(")");
             KeyTerm lBrace = ToTerm("{");
             KeyTerm rBrace = ToTerm("}");
+            var lSqBracket = ToTerm("[");
+            var rSqBracket = ToTerm("]");
             KeyTerm semi = ToTerm(";");
             KeyTerm comma = ToTerm(",");
             KeyTerm dot = ToTerm(".");
@@ -47,6 +49,7 @@ namespace MintLanguage
             NonTerminal parameter = new NonTerminal("parameter");
 
             NonTerminal block = new NonTerminal("block");
+            NonTerminal statementExp = new NonTerminal("statement-exp");
             NonTerminal statement = new NonTerminal("statement");
             NonTerminal statementListOpt = new NonTerminal("statement-list-opt");
             NonTerminal declarationStatement = new NonTerminal("declaration-statement");
@@ -63,7 +66,6 @@ namespace MintLanguage
             NonTerminal incOrDec = new NonTerminal("inc-or-dec", ToTerm("++") | "--");
 
             NonTerminal expression = new NonTerminal("expression");
-            NonTerminal identifierOrTypecast = new NonTerminal("identifierOrTypecast");
             NonTerminal typecast = new NonTerminal("typecast");
             NonTerminal primaryExp = new NonTerminal("primary-expression");
             NonTerminal binOpExp = new NonTerminal("binary-expression");
@@ -71,6 +73,7 @@ namespace MintLanguage
             NonTerminal parenthExp = new NonTerminal("parenthesized-expression");
             NonTerminal preIncDecExp = new NonTerminal("pre-inc/dec");
             NonTerminal postIncDecExp = new NonTerminal("post-inc/dec");
+            var localizeExp = new NonTerminal("localize");
 
             NonTerminal memberAccess = new NonTerminal("member-access");
             NonTerminal memberAccessSegment = new NonTerminal("member-access-segment");
@@ -79,6 +82,27 @@ namespace MintLanguage
             NonTerminal methodInvocation = new NonTerminal("method-invocation");
             NonTerminal argList = new NonTerminal("arg-list");
             NonTerminal arg = new NonTerminal("arg");
+
+            var selectionStatement = new NonTerminal("selection");
+            var ifStatement = new NonTerminal("if");
+            var switchStatement = new NonTerminal("switch");
+            var elseClauseOpt = new NonTerminal("else-clause-opt");
+            var switch_section = new NonTerminal("switch_section");
+            var switch_sections_opt = new NonTerminal("switch_sections_opt");
+            var switch_label = new NonTerminal("switch_label");
+            var switch_labels = new NonTerminal("switch_labels");
+            var while_statement = new NonTerminal("while_statement");
+            var do_statement = new NonTerminal("do_statement");
+            var for_statement = new NonTerminal("for_statement");
+            var foreach_statement = new NonTerminal("foreach_statement");
+            var for_initializer_opt = new NonTerminal("for_initializer_opt");
+            var for_condition_opt = new NonTerminal("for_condition_opt");
+            var for_iterator_opt = new NonTerminal("for_iterator_opt");
+            var break_statement = new NonTerminal("break_statement");
+            var continue_statement = new NonTerminal("continue_statement");
+            var goto_statement = new NonTerminal("goto_statement");
+            var return_statement = new NonTerminal("return_statement");
+
             #endregion
 
             #region operators, punctuation and delimiters
@@ -96,9 +120,9 @@ namespace MintLanguage
             // RegisterOperators(12, "++", "--");
             RegisterOperators(-3, "=", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=");
 
-            MarkPunctuation(lPar, rPar, lBrace, rBrace, comma, semi, dot);
-            this.MarkTransient(declaration, statement, embeddedStatement, expression, literal, binaryOperator, primaryExp, parenParameters,
-                declarationStatement, incOrDec, parenthExp);
+            MarkPunctuation(lPar, rPar, lBrace, rBrace, lSqBracket, rSqBracket, comma, semi, dot);
+           // this.MarkTransient(declaration, statement, embeddedStatement, expression, literal, binaryOperator, primaryExp, parenParameters,
+           //     declarationStatement, incOrDec, parenthExp);
 
             this.AddTermsReportGroup("assignment", "=", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=");
             this.AddTermsReportGroup("statement", "if", "switch", "do", "while", "for", "foreach", "continue", "goto", "return", "try", "yield",
@@ -137,6 +161,7 @@ namespace MintLanguage
             block.Rule = lBrace + statementListOpt + rBrace;
             statementListOpt.Rule = MakeStarRule(statementListOpt, null, statement);
             statement.Rule = declarationStatement | embeddedStatement;
+            statementExp.Rule = memberAccess | memberAccess + assignmentOperator + expression | preIncDecExp | postIncDecExp;
 
             // Decalartion statement
             declarationStatement.Rule = variableDeclaration + semi;
@@ -146,17 +171,32 @@ namespace MintLanguage
             initializer.Rule = expression;
 
             // Embedded statement
-            embeddedStatement.Rule = block | semi;
+            embeddedStatement.Rule = block | semi | statementExp | selectionStatement;
+
+            // Selection
+            selectionStatement.Rule = ifStatement; //| switchStatement;
+            
+            // If else
+            ifStatement.Rule = ToTerm("if") + parenthExp + embeddedStatement + elseClauseOpt;
+            elseClauseOpt.Rule = Empty | PreferShiftHere() + "else" + embeddedStatement;
+
+            // switch
+            switchStatement.Rule = "switch" + parenthExp + lBrace + switch_sections_opt + rBrace;
+            //switch_section.Rule = switch_labels + statement_list;
+            //switch_sections_opt.Rule = MakeStarRule(switch_sections_opt, null, switch_section);
+            //switch_label.Rule = "case" + expression + colon | "default" + colon;
+            //switch_labels.Rule = MakePlusRule(switch_labels, null, switch_label);
 
             // expression
             expression.Rule = typecast | binOpExp | primaryExp;
             typecast.Rule = parenthExp + primaryExp;
             binOpExp.Rule = expression + binaryOperator + expression;
-            primaryExp.Rule = literal | identifier | memberAccess | unaryExp | parenthExp | preIncDecExp | postIncDecExp;
+            primaryExp.Rule = literal | identifier | memberAccess | unaryExp | parenthExp | preIncDecExp | postIncDecExp | localizeExp;
             unaryExp.Rule = unaryOperator + primaryExp;
             parenthExp.Rule = lPar + expression + rPar;
             preIncDecExp.Rule = incOrDec + memberAccess;
             postIncDecExp.Rule = memberAccess + incOrDec;
+            localizeExp.Rule = lSqBracket + doubleString + rSqBracket;
 
             memberAccess.Rule = identifier + memberAccessSegments;
             memberAccessSegments.Rule = MakePlusRule(memberAccessSegments, null, memberAccessSegment);
